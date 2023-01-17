c8e0ab77672b02df2bb7b545271482955b5b22bb
Danil <GromPuzan@proton.me>
2023-01-17 12:28:36 +0300

    feat: add hamburger menu

--- plants/index.js
+++ plants/index.js
@@ -1,1 +1,18 @@
 console.log('1.Вёрстка валидная +10.\n2.Вёрстка семантическая +20.\n3.Вёрстка соответствует макету +48.\n4.Требования к css + 12.\n5.Интерактивность, реализуемая через css +20.\nИтого:110 баллов.')
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav-list'),
+    navItem = document.querySelectorAll('.nav-item'),
+    hamburger = document.querySelector('.hamb');
+    bar = document.querySelector('.bar');
+
+    hamburger.addEventListener('click', () => {
+        bar.classList.toggle('bar_active');
+        menu.classList.toggle('nav-list_active');
+    });
+
+    navItem.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.toggle('nav-list_active');
+        })
+    })
+})
